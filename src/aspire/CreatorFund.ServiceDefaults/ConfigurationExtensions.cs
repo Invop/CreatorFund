d39872b0693d2216f464ab22f21e8c69b89cac66
Andrii Shandryk <shandrykjj512@gmail.com>
2025-03-11 23:48:03 +0200

    Add extension method to retrieve required configuration values
    Introduces a `GetRequiredValue` method for `IConfiguration` to ensure mandatory configuration values are retrieved or throw an exception if missing. This helps enforce stricter runtime validation of configuration settings.

---
+++ src/aspire/CreatorFund.ServiceDefaults/ConfigurationExtensions.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.Hosting;
+
+public static class ConfigurationExtensions
+{
+    public static string GetRequiredValue(this IConfiguration configuration, string name)
+    {
+        return configuration[name] ?? throw new InvalidOperationException(
+            $"Configuration missing value for: {(configuration is IConfigurationSection s ? s.Path + ":" + name : name)}");
+    }
+}
