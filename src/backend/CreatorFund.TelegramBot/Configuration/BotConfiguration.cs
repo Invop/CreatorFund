17ab4b05e0bd62864e1ee36b125498c6e3d5a11f
Andrii Shandryk <shandrykjj512@gmail.com>
2025-03-14 01:45:44 +0200

    Add ChatId property to BotConfiguration
    Introduce a new `ChatId` property in the `BotConfiguration` class to store the chat identifier. Updated the `appsettings.json` file to include a default value for `ChatId`. This change enables better support for bot functionalities requiring specific chat references.

--- src/backend/CreatorFund.TelegramBot/Configuration/BotConfiguration.cs
+++ src/backend/CreatorFund.TelegramBot/Configuration/BotConfiguration.cs
@@ -3,5 +3,6 @@ namespace CreatorFund.TelegramBot.Configuration;
 public class BotConfiguration
 {
     public string BotToken { get; init; } = default!;
-    public long AdminId { get; init; } = default!;
+    public long AdminId { get; init; }
+    public long ChatId { get; init; }
 }
