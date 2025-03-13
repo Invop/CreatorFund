8930f308d2c287c2b7cf53a37a388d4424a205b8
Andrii Shandryk <shandrykjj512@gmail.com>
2025-03-13 21:55:22 +0200

    Add RabbitMQ management plugin to the app setup

--- src/aspire/CreatorFund.AppHost/Program.cs
+++ src/aspire/CreatorFund.AppHost/Program.cs
@@ -3,7 +3,9 @@ using Projects;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var redis = builder.AddRedis("redis");
+//TODO: ADD rabbitmq-delayed-message-exchange plugin
 var rabbitMq = builder.AddRabbitMQ("eventbus")
+    .WithManagementPlugin()
     .WithLifetime(ContainerLifetime.Persistent);
 var postgres = builder.AddPostgres("postgres")
     .WithPgAdmin()
